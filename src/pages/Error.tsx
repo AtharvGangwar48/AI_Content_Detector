7c2945a541c17542adea17d203009364085eae61
Atharv Gangwar <152419242+AtharvGangwar48@users.noreply.github.com>
2025-06-17 20:27:20 +0530

    incorrect route handling

---
+++ src/pages/Error.tsx
@@ -0,0 +1,9 @@
+function Error() {
+    return ( 
+        <>
+        <h1 style={{textAlign:'center', margin:'300px', fontSize:'30px'}}>Error 404! Page Not Found</h1>
+        </>
+     );
+}
+
+export default Error;
